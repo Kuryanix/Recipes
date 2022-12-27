62ee4448d4d208b964acda8a484cedf784938a39
Максим <99maks@bk.ru>
2022-12-27 19:06:51 +0300

    дз (подключение библиотек) готово

--- src/main/java/com/example/web3/model/Ingredient.java
+++ src/main/java/com/example/web3/model/Ingredient.java
@@ -1,32 +1,12 @@
 package com.example.web3.model;
 
+import lombok.Data;
+
+@Data
 public class Ingredient {
 
     private String title;
     private int quantity;
     private String measureUnit;
 
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public String getMeasureUnit() {
-        return measureUnit;
-    }
-
-    public void setMeasureUnit(String measureUnit) {
-        this.measureUnit = measureUnit;
-    }
 }
